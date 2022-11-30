02fbb1714c0ab9d1a80a24c274052a1f26eaaf64
Dmitry Sedov <DSedov@ppfinsurance.ru>
2022-11-30 16:41:38 +0300

    refactor menu generator code

--- src/Helpers/MenuGenerator.php
+++ src/Helpers/MenuGenerator.php
@@ -2,6 +2,8 @@
 
 namespace Cptbadcode\LaravelPager\Helpers;
 
+use Cptbadcode\LaravelPager\Contracts\IMenu;
+use Cptbadcode\LaravelPager\Contracts\Menu\{IMenuItem, IMenuDirectory};
 use Cptbadcode\LaravelPager\Menu\{Menu, MenuDirectory};
 use Cptbadcode\LaravelPager\PageService;
 
@@ -17,31 +19,65 @@ class MenuGenerator
         foreach ($iterator as $splFileInfo) {
             if ($iterator->isDot()) continue;
 
-            if ($splFileInfo->isDir()) {
-                $title = $splFileInfo->getFilename();
-                $path = new MenuDirectory($title, $title);
-            }
-            else {
-                $page = new (get_class_from_file($splFileInfo));
-                $page = PageService::repository()->getPage($page->getKey()) ?? $page;
-                if (!$page->canAddToMenu()) continue;
-                $path = $page->forMenu();
-            }
+            if (!$path = self::getMenuObject($splFileInfo)) continue;
 
-            for ($depth = $iterator->getDepth() - 1; $depth >= 0; $depth--) {
-                $titleDir = $iterator->getSubIterator($depth)->current()->getFilename();
-                $dir = $menu->find($titleDir);
-                if ($dir) {
-                    if ($menu->find($path->key)) continue;
-                    $dir->addItem($path);
+            $path = self::reduceInDepth($path, $iterator, $menu);
 
-                }
-                else {
-                    $path = new MenuDirectory($titleDir, $titleDir, [$path]);
-                }
-            }
             if(!$menu->find($path->key)) $menu->add($path);
         }
         return $menu;
+    }
+
+    /**
+     * Получить директорию или елемент меню из прочитанного файла
+     * @param \SplFileInfo $splFileInfo
+     * @return IMenuItem|MenuDirectory|null
+     */
+    private static function getMenuObject(\SplFileInfo $splFileInfo): IMenuItem|MenuDirectory|null
+    {
+        $title = $splFileInfo->getFilename();
+        return $splFileInfo->isDir()
+            ? new MenuDirectory($title, $title)
+            : self::getMenuItem($splFileInfo);
+    }
+
+    /**
+     * @param \SplFileInfo $splFileInfo
+     * @return IMenuItem|null
+     */
+    private static function getMenuItem(\SplFileInfo $splFileInfo): ?IMenuItem
+    {
+        $page = new (get_class_from_file($splFileInfo));
+        $page = PageService::repository()->getPage($page->getKey()) ?? $page;
+        if (!$page->canAddToMenu()) return null;
+        return $page->forMenu();
+    }
+
+    /**
+     * Просмотреть директории от текущей к родительской для формирования
+     * вложенности меню сохраняя иерархию папок
+     *
+     * @param IMenuDirectory|IMenuItem $item
+     * @param \RecursiveIteratorIterator $iterator
+     * @param IMenu $menu
+     * @return IMenuDirectory|IMenuItem
+     */
+    private static function reduceInDepth(
+        IMenuDirectory|IMenuItem $item,
+        \RecursiveIteratorIterator $iterator,
+        IMenu $menu
+    ): IMenuDirectory|IMenuItem
+    {
+        for ($depth = $iterator->getDepth() - 1; $depth >= 0; $depth--) {
+            $titleDir = $iterator->getSubIterator($depth)->current()->getFilename();
+            $dir = $menu->find($titleDir); // найти в текущей версии меню, директорию родителя
+            if ($dir) {
+                if ($menu->find($item->key)) continue; // добавлен ли уже элемент в меню
+                $dir->addItem($item);
+            }
+            else $item = new MenuDirectory($titleDir, $titleDir, [$item]);
+        }
+
+        return $item;
     }
 }
